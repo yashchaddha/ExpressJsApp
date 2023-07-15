fb718366770f75616ca8d18d7e1bc54980c14a17
Yash Chaddha <yash.chaddha1997@gmail.com>
2023-07-16 03:49:16 +0530

    Contacts are being added now

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,24 +1,31 @@
-module.exports.todo=function(req,res){
-    var tasks=[
-        {
-            description:"Go to Gym",
-            date:new Date().toISOString().split('T')[0],
-            priority:"High"
-        },
-        {
-            description:"Read Books",
-            date:new Date().toISOString().split('T')[0],
-            priority:"Regular"
-        },
-        {
-            description:"Do More Open Source Contributions",
-            date:new Date().toISOString().split('T')[0],
-            priority:"Critical"
-        }]
-    
-    console.log(res.body);
+var tasks=[
+    {
+        description:"Go to Gym",
+        date:new Date().toISOString().split('T')[0],
+        priority:"High"
+    },
+    {
+        description:"Read Books",
+        date:new Date().toISOString().split('T')[0],
+        priority:"Regular"
+    },
+    {
+        description:"Do More Open Source Contributions",
+        date:new Date().toISOString().split('T')[0],
+        priority:"Critical"
+    }]
 
+module.exports.todo=function(req,res){
     return res.render('todo',{
         todotasks:tasks
     })
+}
+
+module.exports.addTasks=function(req,res){
+    tasks.push({
+        description:req.body.taskDescription,
+        Date: new Date().toISOString().split('T')[0],
+        priority:"Regular"
+    })
+    return res.redirect('back');
 }
--- routes/todo.js
+++ routes/todo.js
@@ -2,11 +2,9 @@ const express=require('express');
 const router=express.Router();
 
 
-
 const todoController=require('../controllers/todo_controller');
-const addtodotasks=require('../controllers/add_todo');
 
 router.get('/',todoController.todo);
-router.post('/create-task',addtodotasks.addTodo);
+router.post('/create-task',todoController.addTasks);
 
 module.exports=router
