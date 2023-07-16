8be8dedddd329088fb0de3ee06792c6ea24e1bb5
Yash Chaddha <yash.chaddha1997@gmail.com>
2023-07-16 08:15:59 +0530

    Styling Done

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -2,17 +2,20 @@ var tasks=[
     {
         description:"Go to Gym",
         date:new Date().toISOString().split('T')[0],
-        priority:"High"
+        priority:"High",
+        category:"Personal"
     },
     {
         description:"Read Books",
         date:new Date().toISOString().split('T')[0],
-        priority:"Regular"
+        priority:"Regular",
+        category:"Personal"
     },
     {
         description:"Do More Open Source Contributions",
         date:new Date().toISOString().split('T')[0],
-        priority:"Critical"
+        priority:"Critical",
+        category:"Work"
     }]
 
 module.exports.todo=function(req,res){
@@ -25,7 +28,8 @@ module.exports.addTasks=function(req,res){
     tasks.push({
         description:req.body.taskDescription,
         date: req.body.datepicker,
-        priority:req.body.priority
+        priority:req.body.priority,
+        category:req.body.category
     })
     return res.redirect('back');
 }
