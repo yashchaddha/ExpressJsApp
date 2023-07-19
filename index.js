67d7da27f52daef2acf08414f4a531521026a60c
Yash Chaddha <yash.chaddha1997@gmail.com>
2023-07-19 17:21:58 +0530

    Creating Routes and Controllers for User Authentication

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const port=8080;
+const port=8000;
 const db=require('./config/mongoose');
 const expressLayouts=require('express-ejs-layouts');
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,10 @@ const router=express.Router();
 const userController=require('../controllers/user_controller')
 
 router.get('/profile',userController.profile);
+router.get('/signup',userController.signup);
+router.get('/login',userController.login)
+router.post('/create-user',userController.createUser);
+router.post('/create-session',userController.createSession);
 
 
 module.exports=router
